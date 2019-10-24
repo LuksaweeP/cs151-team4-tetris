157db6e18181ea492530aa8d24f3b32b684e919f
LuksaweeP <luksawee.phansri@gmail.com>
2019-10-23 22:19:45 -0700

    Add playFieldTest

---
+++ src/model/PlayfieldTest.java
@@ -0,0 +1,61 @@
+package model;
+
+public class PlayfieldTest 
+{
+	public static void main(String[] args)
+	{
+	char [] array = {'I', 'O', 'T', 'S', 'Z', 'L', 'J'};
+	int number = array.length;
+	
+	Playfield playfield = new Playfield();
+	int [][] screen = playfield.getGrid();
+	
+	Tetromino curr = new Tetromino('J');
+	
+	//curr.setBlocks();
+	curr.getBlocks();
+	
+	curr.moveRight();
+	curr.moveRight();
+	curr.moveRight();
+	curr.moveRight();
+	
+	
+	curr.moveDown();
+	curr.moveDown();
+	curr.moveDown();
+	curr.moveDown();
+	
+	curr.rotateLeft();
+	curr.rotateLeft();
+	curr.rotateLeft();
+	curr.rotateLeft();
+	
+	System.out.println(curr.getShape());
+	
+	Block [] block = new Block[4];
+	block = curr.getBlocks();
+	
+	for(int i = 0; i < block.length; i++)
+	{
+		int x = block[i].getXPosition();
+		int y = block[i].getYPosition();
+		System.out.println(x + " , " + y);
+	}
+	
+	for(int i = 0; i < block.length; i++)
+	{
+		screen[block[i].getYPosition()][block[i].getXPosition()] = 1;
+	}
+	
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			System.out.print(screen[i][j] + "  ");
+		}
+		System.out.println();
+	}
+	}
+}
+
