18ca87865c1124b099d900ffab59f385fedada20
NickBurnam <nickburnam789@gmail.com>
2019-10-26 12:16:39 -0700

    Initial Creation

---
+++ src/edu/sjsu/cs151/tetris/model/ModelTest.java
@@ -0,0 +1,17 @@
+package edu.sjsu.cs151.tetris.model;
+
+/**
+ * A Class to test the Tetris game logic implemented in the edu.sjsu.cs151.tetris.model package
+ */
+public class ModelTest {
+
+	/**
+	 * The main method to test the game logic.
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
