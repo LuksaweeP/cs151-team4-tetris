b974fe0293fbfd397a46829c9de75c61671a81b2
LuksaweeP <luksawee.phansri@gmail.com>
2019-11-26 16:13:43 -0800

    Edit Controller

--- src/edu/sjsu/cs151/tetris/controller/Controller.java
+++ src/edu/sjsu/cs151/tetris/controller/Controller.java
@@ -1,22 +1,15 @@
 package edu.sjsu.cs151.tetris.controller;
 
-import java.util.LinkedList;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
-import com.sun.xml.internal.ws.api.message.Message;
-
-import edu.sjsu.cs151.tetris.model.Playfield;
+import edu.sjsu.cs151.tetris.model.Model;
 import edu.sjsu.cs151.tetris.view.View;
 
 
-public class Controller implements Runnable
+public class Controller
 {
-	public Controller(View view, Playfield playfield, LinkedBlockingQueue<Message> queue)
+	public Controller(View view, Model model)
 	{
 		this.view = view;
-		this.playfield = playfield;
-		this.queue = queue;
+		this.model = model;
 	}
 	
 	public static void mainLoop() throws Exception
@@ -38,8 +31,7 @@ public class Controller implements Runnable
 	}
 	
 	private View view;
-	private Playfield playfield;
-	static LinkedBlockingQueue<Message> queue;
+	private Model model;
 	
 }
 
