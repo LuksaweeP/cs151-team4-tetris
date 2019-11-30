c19d8ee772cb410de1ef911b88966d801f949eb0
LuksaweeP <luksawee.phansri@gmail.com>
2019-11-30 13:34:34 -0800

    add class to use to calculate logic to the tetromino and playfield

---
+++ src/edu/sjsu/cs151/tetris/model/Figure.java
@@ -0,0 +1,71 @@
+package edu.sjsu.cs151.tetris.model;
+
+/**
+ * A class for creating testrominos which will use in GameRule class.
+ * @author Luksawee
+ */
+
+public class Figure 
+{
+		public static final Block[][][] FiguresList = {
+				
+			// J-Figure
+			{
+				{ new Block(0, 1), new Block(1, 1), new Block(2, 1), new Block(2, 2) },
+				{ new Block(1, 0), new Block(1, 1), new Block(1, 2), new Block(0, 2) },
+				{ new Block(0, 1), new Block(1, 1), new Block(2, 1), new Block(0, 0) },
+				{ new Block(1, 0), new Block(1, 1), new Block(1, 2), new Block(2, 0) }
+			},
+			
+			// Z-Figure
+			{
+				{ new Block(0, 0), new Block(1, 0), new Block(1, 1), new Block(2, 1) },
+				{ new Block(1, 0), new Block(0, 1), new Block(1, 1), new Block(0, 2) },
+				{ new Block(0, 0), new Block(1, 0), new Block(1, 1), new Block(2, 1) },
+				{ new Block(1, 0), new Block(0, 1), new Block(1, 1), new Block(0, 2) }
+			},
+			
+			
+			// L-Figure
+			{
+				{ new Block(0, 1), new Block(1, 1), new Block(2, 1), new Block(2, 0) },
+				{ new Block(1, 0), new Block(1, 1), new Block(1, 2), new Block(2, 2) },
+				{ new Block(0, 1), new Block(1, 1), new Block(2, 1), new Block(0, 2) },
+				{ new Block(1, 0), new Block(1, 1), new Block(1, 2), new Block(0, 0) }
+			},
+		
+			
+			// S-Figure
+			{
+				{ new Block(1, 0), new Block(2, 0), new Block(0, 1), new Block(1, 1) },
+				{ new Block(0, 0), new Block(0, 1), new Block(1, 1), new Block(1, 2) },
+				{ new Block(1, 0), new Block(2, 0), new Block(0, 1), new Block(1, 1) },
+				{ new Block(0, 0), new Block(0, 1), new Block(1, 1), new Block(1, 2) }
+			},
+			
+			
+			// I-Figure
+			{
+				{ new Block(0, 1), new Block(1, 1), new Block(2, 1), new Block(3, 1) },
+				{ new Block(1, 0), new Block(1, 1), new Block(1, 2), new Block(1, 3) },
+				{ new Block(0, 1), new Block(1, 1), new Block(2, 1), new Block(3, 1) },
+				{ new Block(1, 0), new Block(1, 1), new Block(1, 2), new Block(1, 3) }
+			},
+			
+			// O-Figure
+			{
+				{ new Block(0, 0), new Block(0, 1), new Block(1, 0), new Block(1, 1) },
+				{ new Block(0, 0), new Block(0, 1), new Block(1, 0), new Block(1, 1) },
+				{ new Block(0, 0), new Block(0, 1), new Block(1, 0), new Block(1, 1) },
+				{ new Block(0, 0), new Block(0, 1), new Block(1, 0), new Block(1, 1) }
+			},
+			
+			// T-Figure
+			{
+				{ new Block(1, 0), new Block(0, 1), new Block(1, 1), new Block(2, 1) },
+				{ new Block(1, 0), new Block(0, 1), new Block(1, 1), new Block(1, 2) },
+				{ new Block(0, 1), new Block(1, 1), new Block(2, 1), new Block(1, 2) },
+				{ new Block(1, 0), new Block(1, 1), new Block(2, 1), new Block(1, 2) }
+			}
+	};
+}
