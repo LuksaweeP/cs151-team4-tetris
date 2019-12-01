ce94808d711fe0127c3f1337bfa82d57132ff207
LuksaweeP <luksawee.phansri@gmail.com>
2019-12-01 14:35:50 -0800

    add main class

---
+++ src/edu/sjsu/cs151/tetris/game/Main.java
@@ -0,0 +1,67 @@
+package edu.sjsu.cs151.tetris.game;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import edu.sjsu.cs151.tetris.controller.Controller;
+import edu.sjsu.cs151.tetris.controller.Message;
+import edu.sjsu.cs151.tetris.controller.TimeController;
+import edu.sjsu.cs151.tetris.model.Model;
+import edu.sjsu.cs151.tetris.view.View;
+
+public class Main {
+
+	private View view;
+	private Model model;
+	private Controller controller;
+	private TimeController tController;
+
+	private Thread controllerThread;
+	private Thread viewThread;
+	private Thread tControllerThread;
+
+	/* Creating BlockongQueues */
+	private BlockingQueue<Message> controllerToViewQueue = new ArrayBlockingQueue<>(1024);
+	private BlockingQueue<Message> viewToControllerQueue = new ArrayBlockingQueue<>(1024);
+
+	public Main() {
+		/* Creating MVC (with 2 controllers) */
+		model = new Model();
+		view = new View();
+		controller = new Controller();
+		tController = new TimeController();
+
+		/* Passing BlockingQueues and model to Controller */
+		controller.setModel(model);
+		controller.setControllerToViewQueue(controllerToViewQueue);
+		controller.setViewToControllerQueue(viewToControllerQueue);
+
+		/*
+		 * TimeController does not need viewToController BlockingQueue because it does
+		 * not interact with user
+		 */
+		tController.setModel(model, view);
+		tController.setControllerToViewQueue(controllerToViewQueue);
+
+		/*
+		 * View does not need Model (data) because all it needs it receives from
+		 * messages
+		 */
+		view.setControllerToViewQueue(controllerToViewQueue);
+		view.setViewToControllerQueue(viewToControllerQueue);
+
+		/* Creating and running threads */
+		controllerThread = new Thread(controller);
+		viewThread = new Thread(view);
+		tControllerThread = new Thread(tController);
+		controllerThread.start();
+		viewThread.start();
+		tControllerThread.start();
+	}
+	
+	public static void main(String[] args) {
+		/* Creating Main class object starts the game */
+		Main Tetris = new Main();
+	}
+}
