d1c49cbe42072ffb7bc89d8d686d227d56a3d7c1
LuksaweeP <luksawee.phansri@gmail.com>
2019-12-02 00:30:03 -0800

    test

--- src/edu/sjsu/cs151/tetris/game/Tetris.java
+++ src/edu/sjsu/cs151/tetris/game/Tetris.java
@@ -1,27 +1,13 @@
 package edu.sjsu.cs151.tetris.game;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
-import com.sun.xml.internal.ws.api.message.Message;
-
-import edu.sjsu.cs151.tetris.controller.Controller;
-import edu.sjsu.cs151.tetris.model.Model;
-import edu.sjsu.cs151.tetris.model.Playfield;
-import edu.sjsu.cs151.tetris.view.View;
-import edu.sjsu.cs151.tetris.view.ViewAllPanels;
-
-
 /**
-A class that houses the main method for the Tetris program.
+ * The main method for the Tetris program.
  */
 public class Tetris 
 {
-	/**
-	The Main Method for Tetris.
-	@param args Unused.  
-	 */
- 
-
-
+	public static void main(String[] args) 
+	{
+		/* Creating Main class object starts the game */
+		Main Tetris = new Main();
+	}
 }
