65f2ef3913b487c2ddb29188ea33aa39d1329ca1
LuksaweeP <luksawee.phansri@gmail.com>
2019-12-01 18:00:04 -0800

    add draw to show nextfigure

--- src/edu/sjsu/cs151/tetris/view/BoardGamePanel.java
+++ src/edu/sjsu/cs151/tetris/view/BoardGamePanel.java
@@ -66,6 +66,9 @@ public class BoardGamePanel extends JPanel
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, 400, 800);
 		
+		g.setColor(new Color(176, 196, 222));
+		g.fillRect(401, 0, 200, 800);
+		
 		//this playfield panel
 		for(int i=0; i < 20; ++i)
 			for(int j=0; j <10; ++j)
@@ -75,27 +78,46 @@ public class BoardGamePanel extends JPanel
 			}
 		
 		// next testromino
-		for(int i=0; i<4; ++i)
+		for(int i = 0; i < 4; ++i)
 			for(int j=0; j<4; ++j)
 			{
 				g.setColor(VisibleFigure.FiguresList[nextShape][i][j]);
-				g.fillRect(402 + 25*j, 30 + 25*i, 23, 23);
+				g.fillRect(451 + 25*j, 401 + 25*i, 23, 23);
 			}
 		
+		g.setColor(Color.BLACK);
 		g.setFont(font);
-		g.setColor(Color.WHITE);
-		g.drawString("SCORES", 415, 200);
-		g.drawString(""+scores, 442, 230);
+		g.drawString("Name: " + name , 415, 50);
+		g.drawString("Level: " + level, 415, 75);
+		g.drawString("Score: " + scores, 415, 100);
 		
+		g.setColor(Color.BLACK);
+		g.setFont(font);
+		g.drawString("esc       PAUSE" , 415, 650);
+		g.drawString("<-        Move Left" , 415, 675);
+		g.drawString("<-        Move Right" , 415, 700);
+		g.drawString("z          Rotate" , 415, 725);
+		g.drawString("space   Drop Faster" , 415, 750);
 		
+
 		if(lost)
 		{
 			g.setColor(Color.WHITE);
 			g.setFont(font1);
 			g.drawString("GAME OVER", 50, 300);
 			g.setFont(font);
-			g.drawString("(Press ENTER to play again)", 145, 350);
+			g.drawString("(Press ENTER to play again)", 70, 350);
 		}
+	}
+	
+	public void setPlayerName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setLevel(int level)
+	{
+		this.level = level;
 	}
 	
 	public int getScore()
@@ -106,9 +128,12 @@ public class BoardGamePanel extends JPanel
 	private static final long serialVersionUID = -8715353373678321308L;
 	private Color [][] colors;
 	private int nextShape;
+	private String name;
 	private int scores;
+	private int level;
 	private boolean lost = false;
 	private Font font = new Font("Dialog", Font.BOLD, 18);
-	private Font font1 = new Font("Dialog", Font.BOLD, 70);
+	private Font font1 = new Font("Dialog", Font.BOLD, 50);
+	
 }
 
