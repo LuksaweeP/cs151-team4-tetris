23688105af95aa7124cbe659e856c2c4996ad8d3
LuksaweeP <luksawee.phansri@gmail.com>
2019-12-05 13:30:00 -0800

    add ViewTest

---
+++ src/edu/sjsu/cs151/tetris/test/ViewTest.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cs151.tetris.test;
+
+import edu.sjsu.cs151.tetris.view.PlayerPanel;
+
+public class ViewTest 
+{
+	public static void main(String[] args)
+	{
+		//new View();
+		new PlayerPanel();
+		//new GameOverPanel();
+	}
+}
