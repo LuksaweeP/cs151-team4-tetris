09c2b8aa5d26c93e05db40056e306889467a50b3
LuksaweeP <luksawee.phansri@gmail.com>
2019-12-06 07:01:55 -0800

    add comment

--- src/edu/sjsu/cs151/tetris/controller/GameInfo.java
+++ src/edu/sjsu/cs151/tetris/controller/GameInfo.java
@@ -2,13 +2,23 @@ package edu.sjsu.cs151.tetris.controller;
 
 import edu.sjsu.cs151.tetris.model.Model;
 
+/**
+ * The class that wrap up all the model
+ */
 public class GameInfo 
 {
+	/**
+	 * The construction of GameInfo
+	 */
 	public GameInfo()
 	{
 		this.model = new Model();
 	}
 	
+	/**
+	 * The method to get the model
+	 * @return model
+	 */
 	public Model getModel()
 	{
 		return model;
