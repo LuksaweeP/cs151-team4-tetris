2dcb41877964b6d5fc03b9decf557c8763e8dd18
LuksaweeP <luksawee.phansri@gmail.com>
2019-12-06 09:27:29 -0800

    add comment

--- src/edu/sjsu/cs151/tetris/view/View.java
+++ src/edu/sjsu/cs151/tetris/view/View.java
@@ -5,8 +5,15 @@ import java.awt.Component;
 
 import javax.swing.JFrame;
 
+/**
+ * Class that contain all panel class.
+ * @author Luksawee
+ */
 public class View
 {
+	/**
+	 * The construction of the View
+	 */
 	public View()
 	{
 		frame = new JFrame("TETRIS");
@@ -16,87 +23,152 @@ public class View
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
+	/**
+	 * Method to return frame.
+	 * @return frame
+	 */
 	public JFrame getFrame()
 	{
 		return frame;
 	}
 	
+	/**
+	 * Method to return welcomePanel.
+	 * @return welcomePanel
+	 */
 	public WelcomePanel getWelcomePanel()
 	{
 		return welcomePanel;
 	}
 	
+	/**
+	 * Method to return mainPanel.
+	 * @return mainPanel
+	 */
 	public MainPanel getMainPanel()
 	{
 		return mainPanel;
 	}
 	
+	/**
+	 * Method to return playerPanel.
+	 * @return playerPanel
+	 */
 	public PlayerPanel getPlayerPanel()
 	{
 		return playerPanel;
 	}
 	
+	/**
+	 * Method to return selectLevelPanel.
+	 * @return selectPanel
+	 */
 	public SelectLevelPanel getSelectLevelPanel()
 	{
 		return selectLevelPanel;
 	}
 	
+	/**
+	 * Method to return leaderboardPanel.
+	 * @return leaderboardPanel
+	 */
 	public LeaderboardPanel getLeaderboardPanel()
 	{
 		return leaderboardPanel;
 	}
 	
+	/**
+	 * Method to return quitpanel.
+	 * @return quitPanel.
+	 */
 	public QuitPanel getQuitPanel()
 	{
 		return quitPanel;
 	}
 	
+	/**
+	 * Method to return InGamePanel.
+	 * @return inGamePanel
+	 */
 	public InGamePanel getInGamePanel()
 	{
 		return inGamePanel;
 	}
 	
+	/**
+	 * Method to return gameOverPanel.
+	 * @return gameOverPanel
+	 */
 	public GameOverPanel getGameOverPanel()
 	{
 		return gameOverPanel;
 	}
 	
+	/**
+	 * Method to return controlsPanel.
+	 * @return controlsPanel
+	 */
 	public ControlsPanel getControlsPanel()
 	{
 
 		return controlsPanel;
 	}
 	
+	/**
+	 * Method to return leaderboardInPausePanel.
+	 * @return leaderboardInPausePanel
+	 */ 
 	public LeaderboardInPausePanel getLeaderboardInPausePanel()
 	{
 		return leaderboardInPausePanel;
 	}
 	
+	/**
+	 * Method to return pausePanel
+	 * @return pausePanel
+	 */
 	public PausePanel getPausePanel()
 	{
 		return pausePanel;
 	}
 	
+	/**
+	 * Method to add component into frame
+	 * @param c compponent
+	 */
 	public void add(Component c)
 	{
 		frame.add(c);
 	}
 	
+	/**
+	 * Method to set selectLevel to the new SelectLevelPanel() 
+	 * to let the player can load different name when they play game.
+	 */
 	public void setSelectLevelPanel()
 	{
 		this.selectLevelPanel = new SelectLevelPanel();
 	}
 	
+	/**
+	 * Method to set the player Panel.
+	 */
 	public void setPlayerPanel()
 	{
 		this.playerPanel = new PlayerPanel();
 	}
 	
+	/**
+	 * Method to setLeaderBoard.
+	 */
 	public void setLeaderBoardPanel()
 	{
 		this.leaderboardPanel = new LeaderboardPanel();
 	}
 	
+	/**
+	 * Method to set leaderBoardInPausePanel.
+	 */
 	public void setLeaderBoarInPausePanel()
 	{
 		this.leaderboardInPausePanel = new LeaderboardInPausePanel();
