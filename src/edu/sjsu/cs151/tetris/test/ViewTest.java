6152ac35cfc47260b6ce06f2864c96e5a12a9055
NickBurnam <nickburnam789@gmail.com>
2019-12-08 11:00:19 -0800

    Added Javadoc comment

--- src/edu/sjsu/cs151/tetris/test/ViewTest.java
+++ src/edu/sjsu/cs151/tetris/test/ViewTest.java
@@ -1,9 +1,17 @@
 package edu.sjsu.cs151.tetris.test;
 
 import edu.sjsu.cs151.tetris.view.PlayerPanel;
-
+/**
+ * Class to test the view
+ * @author Luksawee
+ * @author Nick
+ */
 public class ViewTest 
 {
+	/**
+	 * Main method to test View
+	 * @param args unused
+	 */
 	public static void main(String[] args)
 	{
 		//new View();
